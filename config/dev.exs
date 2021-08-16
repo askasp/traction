5c8b0d7df45c3672de7a043fa246f6fe225f2eaa
Aksel Stadler <aksel@stadler.no>
2021-08-16 22:28:01 +0200

    init commit

--- config/dev.exs
+++ config/dev.exs
@@ -75,4 +75,4 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :traction, :discord_key
+config :traction, :discord_key, ""
